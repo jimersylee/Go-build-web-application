3e910ed79b08c26f432ef82afe4ad689b8999927
jimersylee <jimersylee@gmail.com>
2017-09-12 15:30:15 +0800

    simple router

--- my-code/chapter2/gorilla/mux.go
+++ my-code/chapter2/gorilla/mux.go
@@ -1,1 +1,36 @@
-package gorilla
+package main
+
+import(
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
+const(
+	PORT=":8080"
+)
+
+/**
+mux库是路由库,可以自由定义路由
+ */
+func main(){
+	router:=mux.NewRouter()
+	router.HandleFunc("/pages/{id:[0-9]+}",pageHandler)
+	http.Handle("/",router)
+	http.ListenAndServe(PORT,nil)
+
+}
+
+
+func pageHandler(w http.ResponseWriter,r *http.Request){
+	vars:=mux.Vars(r)
+	pageID:=vars["id"]
+	fileName:="files/"+pageID+".html"
+
+	_, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "files/404.html"
+	}
+
+	http.ServeFile(w,r,fileName)
+}
