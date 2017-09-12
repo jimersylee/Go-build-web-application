0acd27d3be1b09d9895a7894e27cdfd13e9b00f9
jimersylee <jimersylee@gmail.com>
2017-09-12 15:30:53 +0800

    simple router

--- my-code/chapter1/staticServer/staticServer.go
+++ my-code/chapter1/staticServer/staticServer.go
@@ -10,7 +10,7 @@ const (
 )
 
 func main() {
-	err:=http.ListenAndServe(PORT, http.FileServer(http.Dir("/home/jimersylee/projects/go/src/go-build-web-application/my-code/ch1/staticServer")))
+	err:=http.ListenAndServe(PORT, http.FileServer(http.Dir("/home/jimersylee/projects/go/src/go-build-web-application/my-code/chapter1/staticServer")))
 	if err!=nil{
 		fmt.Println("start server failed")
 	}
