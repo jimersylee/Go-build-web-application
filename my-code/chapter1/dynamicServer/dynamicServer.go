561c59fe50142490b4276be2eadfe5b1e5e9b120
jimersylee <jimersylee@gmail.com>
2017-09-12 15:20:03 +0800

    simple router

---
+++ my-code/chapter1/dynamicServer/dynamicServer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"fmt"
+)
+
+const(
+	PORT =":8080"
+)
+
+func serveDynamic(w http.ResponseWriter,r *http.Request){
+	response:="The time is "+time.Now().String()
+	fmt.Fprintln(w,response)
+}
+func serveStatic(w http.ResponseWriter,r *http.Request){
+	http.ServeFile(w,r,"static.html")
+}
+
+
+func main(){
+	http.HandleFunc("/static",serveStatic)
+	http.HandleFunc("/",serveDynamic)
+	http.ListenAndServe(PORT,nil)
+
+}
---
+++ my-code/chapter1/staticServer/staticServer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func main() {
+	err:=http.ListenAndServe(PORT, http.FileServer(http.Dir("/home/jimersylee/projects/go/src/go-build-web-application/my-code/ch1/staticServer")))
+	if err!=nil{
+		fmt.Println("start server failed")
+	}
+	fmt.Println("start server successfully")
+}
