0870ee74ec918548e9c73a5936d36b70eb3560e8
jimersylee <jimersylee@gmail.com>
2018-09-30 18:04:52 +0800

    第四章翻译部分

---
+++ my-code/chapter4/using-templates.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	//数据库配置
+	DBHost  = "127.0.0.1"
+	DBPort  = ":3306"
+	DBUser  = "root"
+	DBPass  = "root"
+	DBDbase = "go"
+
+	//web服务器配置
+	PORT = ":8080"
+)
+
+var database *sql.DB
+
+type Page struct {
+	Title   string
+	Content string
+	Date    string
+}
+
+/**
+数据库连接测试
+ */
+func main() {
+	dbConn := fmt.Sprintf("%s:%s@/%s", DBUser, DBPass, DBDbase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("Couldn't to mysql connect")
+		log.Println(err.Error())
+	}
+	log.Println("Connect to mysql successfully")
+	database = db
+
+	//设置路由
+	routes := mux.NewRouter()
+	routes.HandleFunc("/page/{id:[0-9a-zA\\-]+", ServePage)
+	routes.HandleFunc("/page/{guid:[0-9a-zA\\-]+}", ServePage)
+	http.Handle("/", routes)
+	http.ListenAndServe(PORT, nil)
+}
+
+func ServePage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	pageGUID := vars["guid"]
+	thisPage := Page{}
+	fmt.Println("pageID:"+pageID, "guid:"+pageGUID, "time:"+time.Now().String());
+	err := database.QueryRow(
+		"select page_title,page_content,page_date from pages where page_guid=?", pageGUID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+	if err != nil {
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		log.Println("Couldn't get page: +pageID")
+		log.Println(err.Error())
+		return
+	}
+	fmt.Println("time:" + time.Now().String());
+	//使用模板
+	t,_ :=template.ParseFiles("templates/blog.html")
+	t.Execute(w,thisPage)
+}
